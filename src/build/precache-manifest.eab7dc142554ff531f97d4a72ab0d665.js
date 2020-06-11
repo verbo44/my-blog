cf100b5efddc64d56dad65d29a5d1ebb94a05928
Daniel Verbovszky <daniel.verbovszky@gmail.com>
2020-06-11 15:11:03 +0100

    first commit

---
+++ src/build/precache-manifest.eab7dc142554ff531f97d4a72ab0d665.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "3b9f0b2a56a04df46702192aa9063226",
+    "url": "/index.html"
+  },
+  {
+    "revision": "e37334d5c126c44e629c",
+    "url": "/static/css/main.4a9d094a.chunk.css"
+  },
+  {
+    "revision": "a1850b1e29f83404fcfa",
+    "url": "/static/js/2.11aa829e.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/static/js/2.11aa829e.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "e37334d5c126c44e629c",
+    "url": "/static/js/main.1316378e.chunk.js"
+  },
+  {
+    "revision": "36adbc7179ce49d2ac8a",
+    "url": "/static/js/runtime-main.79e3e27f.js"
+  }
+]);
